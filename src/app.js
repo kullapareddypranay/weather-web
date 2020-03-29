33ff32d125f87a44fa60a9b3ffe4de4336f94e0b
praneeth <kpraneethreddy1912@gmail.com>
2020-03-29 19:33:57 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,97 @@
+const path=require('path')
+const express=require('express')
+const hbs=require('hbs')
+const geocode=require('./utils/geocode')
+
+
+const app=express()
+//setting public directory path
+const publicDirectoryPath=path.join(__dirname,'../public')
+//using static public folder
+app.use(express.static(publicDirectoryPath))
+//setting viewing for hbs
+const viewPaths=path.join(__dirname,'../templates/views')
+//setting path for partials that can be reused
+const partialPath=path.join(__dirname,'../templates/partials')
+
+//setting up handler bar for dyanamic pages
+app.set('view engine','hbs')
+app.set('views',viewPaths)
+hbs.registerPartials(partialPath)
+
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'weather app',
+        name:'pranay'
+    })
+})
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'about me',
+        name:'pranay'
+    })
+})
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'help page',
+        contact:'pranay'
+    })
+})
+app.get('/products',(req,res)=>{
+
+    res.send({
+        products:[]
+    })
+})
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.search)
+    {
+        return res.send({
+            error:'enter the address'
+        })
+    }
+    geocode.geocode(req.query.search,(error,data)=>{
+        if(error){
+            return res.send({
+                error:error
+            })
+
+        }
+        geocode.forecast(data.latitude,data.longitude,(error,forecastdata)=>{
+            if(error)
+            {
+               return res.send({
+                   error:error
+               }) 
+            }
+           res.send({
+               forecast:forecastdata,
+               location:data.location
+           })
+        })
+    
+    })
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title:404,
+        name:'pranay',
+        errormsg:'help article not found'
+    })
+})
+
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title:404,
+        name:'pranay',
+        errormsg:'file not found'
+    })
+})
+
+
+app.listen(3000,()=>{
+    console.log('server is started on port 3000')
+})
---
+++ src/utils/geocode.js
@@ -0,0 +1,50 @@
+const request=require('request')
+
+//geocode for latitude and longitude
+const geocode=(address,callback)=>{
+    const url='https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token=pk.eyJ1Ijoia3ByYW5heS0tMSIsImEiOiJjazg4ZnUyYzQwN2xrM2ltcXBobXpocW40In0.gv9Wh6y84h_SF6IE6m49CA'
+    
+    request({url,json:true},(error,response)=>{
+        if(error)
+        {
+             callback('unable to connect to the server',undefined)
+        }else if(response.body.features.length===0){
+            callback('unable to find the location try with different search',undefined)
+        }
+        else
+        {
+            callback(undefined,{
+                latitude:response.body.features[0].center[1],
+                longitude:response.body.features[0].center[0],
+                location:response.body.features[0].place_name
+            })
+        }
+    })
+
+}
+
+//forecast of the wether
+const forecast=(latitude,longitude,callback)=>{
+    const forecasturl='https://api.darksky.net/forecast/f9cb6791f17f64b7ee8a29b695cfa1c1/'+latitude+','+longitude+'?unit=si'
+    request({url:forecasturl,json:true},(error,response)=>{
+        if(error)
+        {
+            callback('unabe to connect to the server')
+        }
+        else if(response.body.error)
+        {
+            callback('try with different latitudes and longitudes')
+        }
+        else{
+            callback(undefined,response.body.daily.data[0].summary +'and '+response.body.currently.temperature)
+        }
+
+    })
+
+}
+
+
+module.exports={
+    geocode:geocode,
+    forecast:forecast
+}
